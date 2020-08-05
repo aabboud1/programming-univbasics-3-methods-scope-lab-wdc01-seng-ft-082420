ce33eb7c04c77eb9d7ae6ffd2546ffdb8d5e810b
immutable-hashmap-7791 <abboudahmed1@gmail.com>
2020-08-05 21:40:21 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,11 +1,10 @@
 def mario
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts phrase = "It's-a me, Mario!"
-  status
 end
 
 def toadstool
-  mario
+  mario(status)
 end
 
 def all_phrases
